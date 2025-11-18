f5c2d39fa5977c26e63e7a9aef41349b3df7f8bc
Samrajya Pratap Rana <phinix0980@gmail.com>
2025-11-18 11:42:12 +0545

    init

--- src/modules/panels/modules/panel_tests/entities/panel_test.entity.ts
+++ src/modules/panels/modules/panel_tests/entities/panel_test.entity.ts
@@ -2,9 +2,10 @@ import { PanelEntity } from '@modules/panels/entities/panel.entity';
 import { TestEntity } from '@modules/test/entities/test.entity';
 import { AppBaseEntity } from '@common/entity/BaseEntity';
 
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, ManyToOne, Unique } from 'typeorm';
 
 @Entity({ name: 'panel_tests' })
+@Unique('UQ_panel_test', ['panel', 'test'])
 export class PanelTestEntity extends AppBaseEntity {
   @ManyToOne(() => PanelEntity)
   panel: PanelEntity;
--- src/modules/test/modules/test_specimen_container/entities/test_specimen_container.entity.ts
+++ src/modules/test/modules/test_specimen_container/entities/test_specimen_container.entity.ts
@@ -1,10 +1,11 @@
 import { ContainerEntity } from '@modules/container/entities/container.entity';
 import { AppBaseEntity } from '@common/entity/BaseEntity';
 import { SpecimenEntity } from '@modules/specimens/entities/specimen.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, ManyToOne, Unique } from 'typeorm';
 import { TestEntity } from '@modules/test/entities/test.entity';
 
 @Entity({ name: 'testSpecimenContainer' })
+@Unique('UQ_test_specimen_container', ['test', 'specimen', 'container']) // Add this decorator
 export class TestSpecimenContainerEntity extends AppBaseEntity {
   @ManyToOne(() => TestEntity)
   test: TestEntity;
